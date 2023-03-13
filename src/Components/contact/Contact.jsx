93b40c0c1159f47c441218e549a384a0cdb7aa7d
Bryanwang666 <51243314+Bryanwang666@users.noreply.github.com>
2023-03-13 18:26:46 -0400

    contact

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -3,28 +3,29 @@ import './contact.scss';
 
 export default function Contact() {
   const [message,setMessage] = useState(false);
-  const [email, setEmail] = useState('');
-  const [messages, setMessages] = useState('');
   const handleSubmit = (e) => {
     e.preventDefault();
+    const formData = { email, message };
     fetch("/submit-form", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, message }),
+      body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
+      .then((response) => response.text())
       .then((data) => {
         console.log(data);
-        setMessage(true)
+        setMessage(true);
       })
       .catch((error) => {
         console.error("Error:", error);
-        alert("Failed to send message.");
+        alert("Error sending message");
       });
   };
   
+  const [email, setEmail] = useState('');
+  const [messages, setMessages] = useState('');
   return (
     <div className="contact"id='contact'>
       <div className="left">
@@ -33,7 +34,7 @@ export default function Contact() {
       <div className="right">
         <h2>Contact</h2>
         <form onSubmit={handleSubmit}>
-          <input  placeholder='Email'type="email"id="email"value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <input  placeholder='Email'type="email"id="email"value={email}onChange={(e) => setEmail(e.target.value)}/>
           <textarea placeholder='Message' value={messages} onChange={(e) => setMessages(e.target.value)}></textarea>
           <button type='submit'>Send</button>
           {message && <span>Thanks, I'ill reply to you ASAP!</span>}
